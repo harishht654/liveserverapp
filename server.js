d4e0e45b46f8b177e771f08dfd7da119ebcc7ba5
harishht654 <59317487+harishht654@users.noreply.github.com>
2021-06-23 04:36:12 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 var url = require("url");
+
 var bodyParser = require('body-parser');
 
-app.use(bodyParser());
 
+
+app.use(bodyParser());
 var clientResponseRef;
-app.get('/*', (req, res) => {
+app.get('*', function (req, res) {
     var pathname = url.parse(req.url).pathname;
 
     var obj = {
@@ -15,32 +18,35 @@ app.get('/*', (req, res) => {
         method: "get",
         params: req.query
     }
-
     io.emit("page-request", obj);
     clientResponseRef = res;
-})
-
-app.post('/*', (req, res) => {
+});
+app.post('*', function (req, res) {
     var pathname = url.parse(req.url).pathname;
 
     var obj = {
         pathname: pathname,
         method: "post",
-        params: req.body
+        params: req.bodt
     }
-
     io.emit("page-request", obj);
     clientResponseRef = res;
-})
+});
 
-io.on('connection', (socket) => {
-    console.log('a node connected');
+io.on("connection", (socket) => {
+    console.info(`Client connected [id=${socket.id}]`);
+    // initialize this client's sequence number
     socket.on("page-response", (response) => {
         clientResponseRef.send(response);
     })
-})
+
+    // when socket disconnects, remove it from the list:
+    io.on("disconnect", () => {
+        console.info(`Client gone [id=${socket.id}]`);
+    });
+});
 
 var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
-http.listen(server_port, () => {
+server.listen(server_port, () => {
     console.log('listening on *:' + server_port);
 })
